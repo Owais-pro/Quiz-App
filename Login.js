b61cffd323ad047dccf76159ab62cf158ff198ce
Rao-Owais <rao.owais2020@gmail.com>
2024-06-06 12:54:31 +0500

    completed

--- Login.js
+++ Login.js
@@ -1,1 +1,18 @@
-const User = 
+const User = JSON.parse(localStorage.getItem('RegUser'));
+console.log(User);
+const Form = document.querySelector('form');
+const UserEmail = document.querySelector('#uemail');
+const UserPassword = document.querySelector('#upassword');
+
+Form.addEventListener('submit' , (event)=>{
+    event.preventDefault();
+    const validation = User.filter((human)=>{
+ return human.Email === UserEmail.value && human.Password === UserPassword.value;
+    })
+
+    if(validation.length === 0){
+        alert("Enter Correct Email & Password...!")
+        return;
+    }
+    window.location = "Main.html"
+})
